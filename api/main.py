f7d1e383eaeadb8736e2e78a19369b60943f9fd4
emerson <xinayespinoza@gmail.com>
2023-09-05 21:50:36 -0300

    layout

--- api/main.py
+++ api/main.py
@@ -1,9 +1,19 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__,  template_folder='../templates',  static_folder='../static')
+
 @app.route("/")
 def inicio():
-  return "Hola mundo"
+  return render_template('index.html')
 
 @app.route("/nosotros")
 def nosotros():
-  return "Bienvenido a nosotros"
+  return render_template('nosotros.html')
+
+@app.route("/contacto")
+def contacto():
+
+  return render_template('contacto.html')
+
+if __name__ == "__main__":
+  app.run(debug=True)
+  
